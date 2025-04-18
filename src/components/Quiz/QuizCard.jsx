9a6085a124bbdf888eb4249292f8fc12f55ae401
NguynKng <harrybin2109@gmail.com>
2025-04-18 19:43:58 +0700

    update quiz

--- src/components/Quiz/QuizCard.jsx
+++ src/components/Quiz/QuizCard.jsx
@@ -4,26 +4,29 @@ import { Gamepad2 } from "lucide-react";
 function QuizCard({ quiz }) {
   const navigate = useNavigate();
 
-  const handlePlayClick = () => {
-    if (quiz && quiz.id) {
-      navigate(`/quiz/play/${quiz.id}`); // ✅ Đã sửa lại path đúng
-    } else {
-      console.error("Quiz không hợp lệ!");
-    }
+  const handlePlayClick = (id) => {
+    navigate(`/quiz/play/${id}`);
   };
 
   return (
-    <div className="bg-white border rounded-xl shadow hover:shadow-lg transition p-4">
-      <h3 className="text-lg font-semibold mb-1">{quiz.title}</h3>
-      <p className="text-sm text-gray-600 mb-2">{quiz.description}</p>
-      <p className="text-sm text-gray-400 mb-4">{quiz.questionCount} câu hỏi</p>
-      <button
-        onClick={handlePlayClick}
-        className="flex items-center gap-2 text-white bg-green-600 hover:bg-green-700 px-4 py-2 rounded-lg cursor-pointer"
+    <div>
+      <div
+        key={quiz._id}
+        className="bg-white border rounded-xl shadow hover:shadow-lg transition p-4 mb-4"
       >
-        <Gamepad2 size={18} />
-        Chơi
-      </button>
+        <h3 className="text-lg font-semibold mb-1">{quiz.title}</h3>
+        <p className="text-sm text-gray-600 mb-2">{quiz.description}</p>
+        <p className="text-sm text-gray-400 mb-4">
+          {quiz.questionCount} câu hỏi
+        </p>
+        <button
+          onClick={() => handlePlayClick(quiz._id)}
+          className="flex items-center gap-2 text-white bg-green-600 hover:bg-green-700 px-4 py-2 rounded-lg cursor-pointer"
+        >
+          <Gamepad2 size={18} />
+          Chơi
+        </button>
+      </div>
     </div>
   );
 }
